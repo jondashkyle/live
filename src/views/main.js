a808e6b10fa5a1729698344d7a4eeb0111a9bd12
jondashkyle <contact@jon-kyle.com>
2017-10-17 12:21:51 -0700

    nice

--- src/views/main.js
+++ src/views/main.js
@@ -6,13 +6,9 @@ var hang = require('./hang')
 module.exports = main
 
 function main(state, emit) {
-  return content()
-
-  function content () {
-    if (state.app.live) {
-      return hang(state, emit)
-    } else {
-      return intermission(state, emit)
-    }
+ if (state.app.live) {
+    return hang(state, emit)
+  } else {
+    return intermission(state, emit)
   }
 }
