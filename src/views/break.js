a78416c31f2f5759d2a2ee8f870bc872c6d72621
jondashkyle <contact@jon-kyle.com>
2017-10-18 13:00:44 -0700

    break

--- src/views/break.js
+++ src/views/break.js
@@ -13,11 +13,7 @@ function view (state, emit) {
   return html`
     <div class="vhmn100 c12 x xjc xac lh1-5">
       <div class="${style}">
-        The next hang is on October 18th (Wednesday) at 11:00am PST.<br><br>
-
-        I’ll be live-coding an ultralight p2p cms inside Beaker Browser, and answering questions related to the future of publishing on the p2p web.<br><br>
-
-        See you then ✌️
+        Thanks ✌️
       </div>
     </div>
   `
