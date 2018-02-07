991cc294afd46e8e408b21c651eb1d108188a99f
jondashkyle <contact@jon-kyle.com>
2018-02-06 22:36:43 -0800

    note

--- src/views/break.js
+++ src/views/break.js
@@ -13,7 +13,7 @@ function view (state, emit) {
   return html`
     <div class="vhmn100 c12 x xjc xac lh1-5">
       <div class="${style}">
-        Thanks ✌️
+        02/07/2018 @ 11AM PST
       </div>
     </div>
   `
